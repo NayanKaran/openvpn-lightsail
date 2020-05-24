03a4d0acbb6205adc879c4949eb2b5a134118d58
NayanKaran <33381784+NayanKaran@users.noreply.github.com>
2020-05-23 22:27:45 -0700

    Update openvpn-install.sh

--- openvpn-install.sh
+++ openvpn-install.sh
@@ -52,7 +52,7 @@ newclient () {
 IP=$(ip addr | grep 'inet' | grep -v inet6 | grep -vE '127\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' | grep -o -E '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' | head -1)
 PUBLIC_IP=$(wget -4qO- "http://whatismyip.akamai.com/")
 PROTOCOL=udp
-PORT=443
+PORT=1194
 CLIENT=lightsail-vpn
 
 # Enable EPEL repository
